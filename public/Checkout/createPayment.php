1cb1c17fac987999f2c8e6d99827822b59030e57
esilvajr <edison.junior@s2it.com.br>
2016-04-27 15:14:43 -0300

    Update keys enum & Update createPayment example for installment limit and max installments

--- public/Checkout/createPayment.php
+++ public/Checkout/createPayment.php
@@ -72,6 +72,20 @@ $payment->addPaymentMethod()->withParameters(
     10.00 // (float) Percent
 );
 
+//Add installments with no interest
+$payment->addPaymentMethod()->withParameters(
+    PagSeguro\Enum\PaymentMethod\Group::CREDIT_CARD,
+    PagSeguro\Enum\PaymentMethod\Config\Keys::MAX_INSTALLMENTS_NO_INTEREST,
+    2 // (int) qty of installment
+);
+
+//Add a limit for installment
+$payment->addPaymentMethod()->withParameters(
+    PagSeguro\Enum\PaymentMethod\Group::CREDIT_CARD,
+    PagSeguro\Enum\PaymentMethod\Config\Keys::MAX_INSTALLMENTS_LIMIT,
+    6 // (int) qty of installment
+);
+
 try {
 
     /**
--- source/Enum/PaymentMethod/Config/Keys.php
+++ source/Enum/PaymentMethod/Config/Keys.php
@@ -34,4 +34,14 @@ class Keys
      * Add a discount percent
      */
     const DISCOUNT_PERCENT = "DISCOUNT_PERCENT";
+
+    /**
+     * Max installments with no interest
+     */
+    const MAX_INSTALLMENTS_NO_INTEREST = "MAX_INSTALLMENTS_NO_INTEREST";
+
+    /**
+     * Installment limit
+     */
+    const MAX_INSTALLMENTS_LIMIT = "MAX_INSTALLMENTS_LIMIT";
 }
